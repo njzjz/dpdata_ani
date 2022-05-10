e667b13970cfdcb804bae76aa97e528f99bcfb45
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-10 19:56:13 -0400

    add ani-1x dataset loader

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ setup(
     name="dpdata-ani",
     version="0.0.1",
     install_requires = [
-        "dpdata>=0.2.5", # 0.2.7
+        "dpdata", # 0.2.7
         "torchani",
         "torch",
         "numpy",
+        "h5py",
     ],
     entry_points = {
         'dpdata.plugins': [
