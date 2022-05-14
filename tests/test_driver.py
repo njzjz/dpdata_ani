bb506ed101a10d512577e426724a3a5bfc58fb7d
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-14 01:26:36 -0400

    fix system

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -5,9 +5,12 @@ import numpy as np
 def test_ani_driver():
     s = dpdata.System(data={
         "atom_names": ["H"],
+        "atom_numbs": [0],
         "atom_types": np.zeros((1,), dtype=int),
         "coords": np.zeros((1, 1, 3), dtype=np.float32),
+        "cells": np.zeros((1, 3, 3), dtype=np.float32)
         "orig": np.zeros(3, dtype=np.float32),
+        "nopbc": True,
     })
     ls = s.predict(driver="ani/1x")
     assert ls.get_natoms() == 1
