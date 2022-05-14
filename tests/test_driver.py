c9a10f1fc43994167920b833dfaca988533eb4be
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-14 01:35:54 -0400

    fix atom_numbs

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -5,7 +5,7 @@ import numpy as np
 def test_ani_driver():
     s = dpdata.System(data={
         "atom_names": ["H"],
-        "atom_numbs": [0],
+        "atom_numbs": [1],
         "atom_types": np.zeros((1,), dtype=int),
         "coords": np.zeros((1, 1, 3), dtype=np.float32),
         "cells": np.zeros((1, 3, 3), dtype=np.float32),
