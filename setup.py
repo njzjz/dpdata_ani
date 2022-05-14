370e80d586da759a2208d8e1bae3374c93dbf30e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-14 01:40:23 -0400

    add unittests (#1)
    * add unittests
    
    * install pytest
    
    * fix requires
    
    * fix pytest cmd
    
    * fix commands
    
    * fix cmd
    
    * fix entrypoints
    
    * Update test_format.py
    
    * fix error
    
    * fix ut
    
    * fix system
    
    * add a comma
    
    * fix atom_numbs

--- setup.py
+++ setup.py
@@ -3,16 +3,23 @@ setup(
     name="dpdata-ani",
     version="0.0.1",
     install_requires = [
-        "dpdata", # 0.2.7
+        "dpdata @ git+https://github.com/deepmodeling/dpdata@devel", # 0.2.7
         "torchani",
         "torch",
         "numpy",
         "h5py",
     ],
+    extras_require = {
+        "test": [
+            "requests",
+            "pytest",
+            "pytest-cov",
+        ],
+    },
     entry_points = {
         'dpdata.plugins': [
             'anidriver=dpdata_ani.anidriver:ANIDriver',
-            'ani1xload=dpdata_ani.ani1x.ANI1xFormat',
+            'ani1xload=dpdata_ani.ani1x:ANI1xFormat',
         ]
     },
     packages = ['dpdata_ani'],
---
+++ tests/test_format.py
@@ -0,0 +1,16 @@
+import dpdata
+import requests
+import pytest
+
+
+@pytest.mark.skip(reason="it consumes too much memory")
+def test_load_ani1x(tmp_path):
+    # download ani1x to fn
+    url = "https://figshare.com/ndownloader/files/18112775"
+    r = requests.get(url)
+    fn = tmp_path / "ani.h5"
+    with open(fn, 'wb') as fw:
+        fw.write(r.content)
+    # dpdata
+    ms = dpdata.MultiSystems().from_ani_1x(fn)
+    assert len(ms)
