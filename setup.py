1048084e47a235989cbb988dc06e7dc31482b3fe
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-14 00:58:33 -0400

    fix entrypoints

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     entry_points = {
         'dpdata.plugins': [
             'anidriver=dpdata_ani.anidriver:ANIDriver',
-            'ani1xload=dpdata_ani.ani1x.ANI1xFormat',
+            'ani1xload=dpdata_ani.ani1x:ANI1xFormat',
         ]
     },
     packages = ['dpdata_ani'],
