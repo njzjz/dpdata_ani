7e4c1d072bb0a0dd3345c440204c3f5cf77434b9
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-14 01:22:42 -0400

    fix ut

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -5,8 +5,9 @@ import numpy as np
 def test_ani_driver():
     s = dpdata.System(data={
         "atom_names": ["H"],
-        "atom_types": np.ones((1,), dtype=int),
+        "atom_types": np.zeros((1,), dtype=int),
         "coords": np.zeros((1, 1, 3), dtype=np.float32),
+        "orig": np.zeros(3, dtype=np.float32),
     })
     ls = s.predict(driver="ani/1x")
     assert ls.get_natoms() == 1
