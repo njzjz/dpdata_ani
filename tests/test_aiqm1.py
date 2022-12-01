82dcf1326feec3e153b1fb0a52519a353b82e2a2
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-12-01 04:06:23 -0500

    add aiqm1
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/test_aiqm1.py
+++ tests/test_aiqm1.py
@@ -4,8 +4,7 @@ import dpdata
 import pytest
 
 
-@pytest.mark.skipif(not shutil.which('mndo'), reason='mndo not found')
-def test_odm2_driver():
+def test_aiqm_nn_driver():
     s = dpdata.System(data={
         "atom_names": ["O"],
         "atom_numbs": [2],
@@ -15,7 +14,24 @@ def test_odm2_driver():
         "orig": np.zeros(3, dtype=np.float32),
         "nopbc": True,
     })
-    ls = s.predict(driver="mndo/odm2")
+    ls = s.predict(driver="aiqm1/nn")
+    assert ls.get_natoms() == 2
+    assert ls.get_nframes() == 1
+    ls.check_data()
+
+
+@pytest.mark.skipif(not shutil.which('mndo'), reason='mndo not found')
+def test_aiqm_driver():
+    s = dpdata.System(data={
+        "atom_names": ["O"],
+        "atom_numbs": [2],
+        "atom_types": np.zeros((2,), dtype=int),
+        "coords": np.array([[[0., 0., 0.], [0., 0., 1.2]]]),
+        "cells": np.zeros((1, 3, 3), dtype=np.float32),
+        "orig": np.zeros(3, dtype=np.float32),
+        "nopbc": True,
+    })
+    ls = s.predict(driver="aiqm1")
     assert ls.get_natoms() == 2
     assert ls.get_nframes() == 1
     ls.check_data()
